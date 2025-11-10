fef8b5da9a6102abae1bbab4f94ee7c8c4ee6c48
Lara Schulze <laraschulze@Mac.fritz.box>
2025-11-10 14:15:59 +0100

    h1

---
+++ h1/H1_main.java
@@ -0,0 +1,29 @@
+package h1;
+
+public class H1_main {
+    public static void main(String[] args) {
+        int zone = 2;
+        double price = 0;
+
+        switch (zone) {
+            case 1: 
+                price = 2.00;
+                break;
+            case 2:
+                price = 2.35;
+                break;
+            case 3:
+            case 4:
+                price = 2.85;
+                break;
+            case 5:
+                price = 3.55;
+                break;
+            default:
+                price = 4.00;
+                break;
+        }
+
+        System.out.println("Preis für Zone  " + zone + " : " + price);
+    }
+}
