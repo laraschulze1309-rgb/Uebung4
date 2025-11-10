5ff31d89b2802ff833199449a161b27b0fbe44ba
Lara Schulze <laraschulze@Mac.fritz.box>
2025-11-10 15:56:07 +0100

    h3

---
+++ h3/H3_main.java
@@ -0,0 +1,17 @@
+package h3;
+
+public class H3_main {
+    public static void main(String[] args) {
+        float celsiusFloat = 24.66412334f;
+        float fahrenheitFloat = 1f;
+        double celsiusDouble = 24.66412334;
+        double fahrenheitDouble = 1;
+
+        fahrenheitFloat = (celsiusFloat * 9 / 5) + 32;
+        fahrenheitDouble = (celsiusDouble * 9 / 5) + 32;
+
+        System.out.println("fahrenheitFloat: " + fahrenheitFloat);
+        System.out.println("fahrenheitDouble: " + fahrenheitDouble);
+}
+
+}
