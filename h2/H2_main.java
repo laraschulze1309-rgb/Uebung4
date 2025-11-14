59079c8475dd816b6d4b7a9bbc0164a2e936de58
Lara Schulze <laraschulze@mac.speedport.ip>
2025-11-14 20:17:21 +0100

    h1

--- h2/H2_main.java
+++ h2/H2_main.java
@@ -2,48 +2,57 @@ package h2;
 
 public class H2_main {
     public static void main(String[] args) {
-        boolean x = true;
-        boolean y = true;
-        boolean a = false; 
-        boolean b = false;
-        boolean c = false;
-        int input = 10;
+        boolean x;
+        boolean y;
+        boolean a;
+        boolean b;
+        boolean c;
+
+        int input = 1;
 
         switch (input) {
-            case 0:
-               x = false;
-               y = false;
-               c = true;
-                break;
-            case 1:
-               x = false;
-                y = true;
-                c = false;
-                break;
-            case 11:
-                x = true;      
-                y = true;
-                c = false;
-                break;
             case 10:
+            case 11:
                 x = true;
-                y = false;
-                c = true;
                 break;
+            default:
+                x = false;
         }
-        
-        if (x == true && y == true) {
-            a = true;
+
+        switch (input) {
+            case 1:
+            case 11:
+                y = true;
+                break;
+            default:
+                y = false;
         }
-        if (x == true || x != y) {
-            b = true;
+
+
+        boolean E1 = x;
+        boolean E2 = y;
+        boolean E3 = (x != y);
+
+        a = (E1 && E2);
+
+        b = (E1 || E3);
+
+        // für c
+        if (x && y) {
+            c = false;
+        } else if (x && !y) {
+            c = true;
+        } else if (!x && y) {
+            c = false;
+        } else { 
+            c = true;
         }
 
         System.out.println("input:" + input);
         System.out.println("x: " + x);
         System.out.println("y: " + y);
         System.out.println("a: " + a);
-        System.out.println("b: " + b);  
+        System.out.println("b: " + b);
         System.out.println("c: " + c);
     }
 }
